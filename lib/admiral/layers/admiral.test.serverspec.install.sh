525012f273dfff13aeec840a5751cb31eeb6b3c8
Gattsu <gattsu.blackswordsman@gmail.com>
2017-05-26 16:32:12 +0200

    Add rake gem during serverspec installation

--- lib/admiral/layers/admiral.test.serverspec.install.sh
+++ lib/admiral/layers/admiral.test.serverspec.install.sh
@@ -2,5 +2,5 @@
 
 mkdir -p "${GEM_CACHE}"
 export GEM_HOME GEM_PATH GEM_CACHE
-/opt/chef/embedded/bin/gem install serverspec specinfra --no-rdoc --no-ri
+/opt/chef/embedded/bin/gem install serverspec specinfra rake --no-rdoc --no-ri
 chown ${username}:${username} -R /tmp/${username}
